ecb4e188e7a84a21a0374656c4548b079bb742f6
Stqo <stefandomitrov136@gmail.com>
2025-01-08 22:47:03 +0200

    Mostly done with the role requests to the admins

--- PathFinder.Services.Data/Implementations/RoleRequestService.cs
+++ PathFinder.Services.Data/Implementations/RoleRequestService.cs
@@ -1,18 +1,86 @@
-﻿using PathFinder.Services.Data.Interfaces;
+﻿using PathFinder.Data.Models.Enums;
+using PathFinder.Data.Models;
+using PathFinder.Services.Data.Interfaces;
 using PathFinder.ViewModels.RoleRequestViewModel;
+using PathFinder.Data.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace PathFinder.Services.Data.Implementations
 {
-    public class RoleRequestService : IRoleRequestService
+    public class RoleRequestService(
+        IRepository<CompanyRoleRequest, int> comapnyRoleRequestRepository, 
+        IRepository<InstitutionRoleRequest, int> institutionRoleRequestRepository) : IRoleRequestService
     {
-        public Task SendComanyRoleRequest(RoleRequestSendViewModel model)
+        public async Task<IEnumerable<RoleRequestInfoViewModel>> GetAllCompanyRoleRequests()
         {
-            throw new NotImplementedException();
+            var requests = await comapnyRoleRequestRepository
+                .GetAllAttached()
+                .Where(r => r.Status == RequestStatus.Pending)
+                .Select(r => new RoleRequestInfoViewModel()
+                {
+                    Id = r.Id,
+                    Sender = r.Sender,
+                    Description = r.Description,
+                    Status = r.Status.ToString()
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return requests;    
         }
 
-        public Task SendInstitutionRoleRequest(RoleRequestSendViewModel model)
+        public async Task<IEnumerable<RoleRequestInfoViewModel>> GetAllInstitutionRoleRequests()
         {
-            throw new NotImplementedException();
+            var requests = await institutionRoleRequestRepository
+                .GetAllAttached()
+                .Where(r => r.Status == RequestStatus.Pending)
+                .Select(r => new RoleRequestInfoViewModel()
+                {
+                    Id = r.Id,
+                    Sender = r.Sender,
+                    Description = r.Description,
+                    Status = r.Status.ToString()
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return requests;
+        }
+
+        public async Task SendComanyRoleRequest(RoleRequestSendViewModel model)
+        {
+            if (model == null)
+            {
+                throw new NullReferenceException("Entity was null!");
+            }
+
+            var request = new CompanyRoleRequest()
+            {
+                Id = model.Id,
+                Sender = model.Sender,
+                Description = model.Description,
+                Status = RequestStatus.Pending
+            };
+
+            await comapnyRoleRequestRepository.AddAsync(request);
+        }
+
+        public async Task SendInstitutionRoleRequest(RoleRequestSendViewModel model)
+        {
+            if (model == null)
+            {
+                throw new NullReferenceException("Entity was null!");
+            }
+
+            var request = new InstitutionRoleRequest()
+            {
+                Id = model.Id,
+                Sender = model.Sender,
+                Description = model.Description,
+                Status = RequestStatus.Pending
+            };
+
+            await institutionRoleRequestRepository.AddAsync(request);
         }
     }
 }
